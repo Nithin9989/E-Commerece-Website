7bbb2ed2ad1b64b35bbd858bdd4a80d4c163b71d
Mounika Siripuram <mounikasiripuram71@gmail.com>
2026-05-29 16:24:55 +0530

    commit

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,36 +2,43 @@ import { useContext } from "react";
 import { ProductContext } from "../Context/ProductContext";
 
 function Cart() {
-    const {
-        totalPrice,
-        cartItems,
-        setCartItems } = useContext(ProductContext);
+    const { totalPrice, cartItems, setCartItems } = useContext(ProductContext);
 
-        const increaseQunatity = (id) =>{
-            const updatedCart = cartItems.map((eachItem)=>{
-                if(eachItem.id === id){
-                    return{
-                        ...eachItem, quantity: eachItem.quantity + 1
-                    }
+    const increaseQunatity = (id) => {
+        const updatedCart = cartItems.map((eachItem) => {
+            eachItem.id === id
+            if (eachItem.id === id) {
+                return {
+                    ...eachItem, quantity: eachItem.quantity + 1
                 }
-                return eachItem
-            });
-            setCartItems(updatedCart)
-        }
+            }
+            return eachItem
+        })
+        setCartItems(updatedCart)
 
-        const decreaseQunatity = (id) =>{
-            const updatedCart = cartItems.map((eachItem)=>{
-                if(eachItem.id === id){
-                    return{
-                        ...eachItem, 
-                        quantity: eachItem.quantity > 1 ? eachItem.quantity - 1 : 1
-                    }
+    }
+    const decreaseQunatity = (id) => {
+        const updatedCart = cartItems.map((eachItem) => {
+            eachItem.id === id
+            if (eachItem.id === id) {
+                return {
+                    ...eachItem, quantity: eachItem.quantity > 1 ? eachItem.quantity - 1 : 1
                 }
-                return eachItem
-            });
-            setCartItems(updatedCart)
-        }
+            }
+            return eachItem
+        })
+        setCartItems(updatedCart)
 
+    }
+    const removeItem = (id) => {
+        const confirm = window.confirm('Are You Sure');
+        if (confirm) {
+            const filteredData = cartItems.filter((eachItem) => (
+                eachItem.id !== id
+            ));
+            setCartItems(filteredData);
+        }
+    }
     return (
         <div className="cart-page">
             <h1>My Cart</h1>
@@ -43,19 +50,24 @@ function Cart() {
                 </div>
                 :
                 cartItems.map((eachItem) => (
-                    <div className="cart-card" key={eachItem.id}>
-                        <img src={eachItem.thumbnail} alt='not-found'
-                        className=""/>
-                        <h2>{eachItem.title}</h2>
-                        <p>{eachItem.price}</p>
-                        <div className="quantity-section">
-                            <button onClick={()=>decreaseQunatity(eachItem.id)}>-</button>
-                            <span>{eachItem.quantity}</span>
-                            <button onClick={()=>increaseQunatity(eachItem.id)}>+</button>
+                    <div className="cart-card">
+                        <img src={eachItem.thumbnail} alt={eachItem.title} />
+                        <div className="cart-content">
+                            <h2>{eachItem.title}</h2>
+                            <p>{eachItem.price}</p>
+
+                            <div className="quantity-section">
+                                <button onClick={() => decreaseQunatity(eachItem.id)}>-</button>
+                                <span>{eachItem.quantity}</span>
+                                <button onClick={() => increaseQunatity(eachItem.id)}>+</button>
+                            </div>
+                            <button className="delete-btn"
+                                onClick={() => removeItem(eachItem.id)}>Remove</button>
                         </div>
                     </div>
                 ))
             }
+            <h2>Total Price : {totalPrice}</h2>
         </div>
     )
 }
